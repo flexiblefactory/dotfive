e556cccecec68de4d701400e1503fc9a1c65420b
flexiblefactory <hal@hals-mbp.home>
2019-10-21 05:36:00 +0100

    AppState class

---
+++ front-end/src/AppState.js
@@ -0,0 +1,32 @@
+import EntitySet from './EntitySet';
+import { decorate, observable, computed } from "mobx"
+
+const children = (cat, cats) => cats.all.filter(c => c.parent === cat.id)
+
+const descendants = (cat, cats) => [
+  cat,
+  ...children(cat, cats).reduce((a, c) => [...a, ...descendants(c, cats)], [])
+]
+
+class AppState {
+
+  items = new EntitySet('item')
+  cats = new EntitySet('category')
+
+  selectedCategory = null;
+
+  get itemsInSelectedCategory() {
+    return this.items.all.filter(i => i.category === this.selectedCategory.id)
+  }
+
+  get allowedParentsForSelectedCategory() {
+    const descendantsAndSelf = new Set(descendants(this.selectedCategory, this.cats))
+    return this.cats.all.filter(c => !descendantsAndSelf.has(c))
+  }
+}
+
+export default decorate(AppState, {
+  selectedCategory: observable,
+  itemsInSelectedCategory: computed,
+  allowedParentsForSelectedCategory: computed
+})
