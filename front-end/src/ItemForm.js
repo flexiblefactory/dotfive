e147a92190f56c2b4b08035bdd4982e55d1f8142
flexiblefactory <hal@hals-mbp.home>
2019-10-21 03:35:25 +0100

    Display Item

---
+++ front-end/src/ItemForm.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        padding: theme.spacing(3, 2),
+        margin: theme.spacing(1),
+    },
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+        display: 'block'
+    },
+    textField: {
+        margin: theme.spacing(1),
+    },
+    dense: {
+        marginTop: theme.spacing(2),
+    },
+    menu: {
+        width: 200,
+    },
+}));
+export default function ItemForm({ cats, newItemState: [values, setValues] }) {
+    const classes = useStyles();
+
+    const handleChange = name => event => {
+        setValues({ ...values, [name]: event.target.value });
+    };
+
+    return (
+        <React.Fragment>
+            <FormControl className={classes.formControl}>
+                <TextField
+                    id="standard-name"
+                    label="Name"
+                    className={classes.textField}
+                    value={values.label}
+                    onChange={handleChange('label')}
+                    margin="normal"
+                />
+            </FormControl>
+            <FormControl className={classes.formControl}>
+                <InputLabel htmlFor="item-cat">
+                    Category
+            </InputLabel>
+                <Select
+                    native
+                    value={values.category || ''}
+                    onChange={handleChange('category')}
+                    inputProps={{
+                        name: 'category',
+                        id: 'item-cat',
+                    }}>
+                    {cats.all.map(c => <option key={c.id} value={c.id}>{c.label}</option>)}
+                </Select>
+            </FormControl>
+        </React.Fragment>)
+}
