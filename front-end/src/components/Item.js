f0d658eed67d1bccea096fd8ddaab5973731bcff
flexiblefactory <hal@hals-mbp.home>
2019-10-21 13:40:31 +0100

    Reformat

--- front-end/src/components/Item.js
+++ front-end/src/components/Item.js
@@ -1,31 +1,25 @@
+import { Paper, Typography } from '@material-ui/core';
+import { observer } from "mobx-react";
 import React from 'react';
-import { Paper, Typography, IconButton } from '@material-ui/core';
-import ModalForm from './ModalForm';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
-import { observer } from "mobx-react"
-import ItemForm from './ItemForm';
 import { useStyles } from '../styles';
 import EditDeletePanel from './EditDeletePanel';
+import ItemForm from './ItemForm';
 
 export default observer(({ item, editItemState, save, handleClick, remove, cats }) => {
   const classes = useStyles();
   return (
-  <Paper className={classes.paper + ' ' + classes.item}>
-    <Typography variant="h5" gutterBottom>
-      {item.label}
-    </Typography>
-    <div className={classes.buttonPanel}>
-
-    <EditDeletePanel
-                        title="Delete item"
-                        handleClick={(handleClickOpen)=>handleClick(handleClickOpen)}
-                        onEdit={save}
-                        onDelete={remove}>
-                        <ItemForm newItemState={editItemState} cats={cats} />
-                      </EditDeletePanel>
-
-
-      
-    </div>
-  </Paper>)})
+    <Paper className={classes.paper + ' ' + classes.item}>
+      <Typography variant="h5" gutterBottom>
+        {item.label}
+      </Typography>
+      <div className={classes.buttonPanel}>
+        <EditDeletePanel
+          title="Delete item"
+          handleClick={(handleClickOpen) => handleClick(handleClickOpen)}
+          onEdit={save}
+          onDelete={remove}>
+          <ItemForm newItemState={editItemState} cats={cats} />
+        </EditDeletePanel>
+      </div>
+    </Paper>)
+})
