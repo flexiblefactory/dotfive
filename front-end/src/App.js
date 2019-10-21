95f666fe9af6cc8ae647a57c2fdc21c7d3307df0
flexiblefactory <hal@hals-mbp.home>
2019-10-21 13:35:59 +0100

    Use EditDeletePanel

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,18 @@
+import { Paper, Typography } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import { observer } from "mobx-react";
 import { useSnackbar } from 'notistack';
 import React from 'react';
 import './App.css';
 import AppState from './AppState';
-import CategoryDetail from './components/CategoryDetail';
+import CategoryForm from './components/CategoryForm';
 import CategoryTree from './components/CategoryTree';
+import EditDeletePanel from './components/EditDeletePanel';
 import Item from './components/Item';
+import ItemForm from './components/ItemForm';
+import ModalForm from './components/ModalForm';
 import { useStyles } from './styles';
-
 const appState = new AppState()
 
 const App = observer(
@@ -22,6 +26,7 @@ const App = observer(
     const [newItem, setNewItem] = newItemState;
     const [newCat, setNewCat] = newCategoryState;
     const [editItem, setEditItem] = editItemState;
+    const [editCat, setEditCat] = editCatState;
     const { items, cats } = appState;
 
     items.onMessage = (message) => enqueueSnackbar(`Item ${message}`)
@@ -42,32 +47,69 @@ const App = observer(
                   setNewCat={setNewCat}>
                 </CategoryTree>
               </Grid>
-              {appState.selectedCategory &&
+              {
+                appState.selectedCategory &&
                 <Grid item>
-                  <CategoryDetail
-                    appState={appState}
-                    newItem={newItem}
-                    editCatState={editCatState}
-                    newItemState={newItemState}>
-                    {appState.itemsInSelectedCategory.map(item => (
-                      <Item
-                        remove={() => {
-                          items.delete(item);
-                        }}
+                  <Paper className={classes.paper + ' ' + classes.category}>
+                    
+                    <ModalForm
+                      save={() => {
+                        items.post({ label: newItem.label, category: newItem.category ? parseInt(newItem.category) : null });
+                        setNewItem({ ...newItem, label: '' })
+
+                      }} button={handleClickOpen => (
+                        <Button className={classes.item + ' ' + classes.newItemButton} variant="outlined" color="primary" onClick={handleClickOpen}>
+                          New item
+                      </Button>
+                      )} title="Create a new item" description="You can add items to categories.">
+                      <ItemForm newItemState={newItemState} cats={cats} />
+                    </ModalForm>
+
+                    <Typography variant="h3" gutterBottom>
+                      {appState.selectedCategory.label}
+                    </Typography>
+                    {appState.itemsInSelectedCategory.length} item{appState.itemsInSelectedCategory.length === 1 ? '' : 's'}
+                    {
+                      appState.itemsInSelectedCategory.map(item => (
+                        <Item
+                          remove={() => {
+                            items.delete(item);
+                          }}
+                          handleClick={(handleClickOpen) => {
+                            setEditItem({ ...item });
+                            handleClickOpen();
+                          }}
+                          item={item}
+                          cats={cats}
+                          key={`item${item.id}`}
+                          editItemState={editItemState}
+                          save={() => {
+                            items.patch(editItem)
+                          }}>
+                        </Item>
+                      ))
+                    }
+
+                    {appState.selectedCategory && appState.selectedCategory.id ?
+
+                      <EditDeletePanel
+                        title="Delete category"
                         handleClick={(handleClickOpen) => {
-                          setEditItem({ ...item });
+                          setEditCat({ ...appState.selectedCategory });
                           handleClickOpen();
                         }}
-                        item={item}
-                        cats={cats}
-                        key={`item${item.id}`}
-                        editItemState={editItemState}
-                        save={() => {
-                          items.patch(editItem)
+                        onEdit={() => {
+                          cats.patch(editCat)
+                        }}
+                        onDelete={() => {
+                          cats.delete(appState.selectedCategory);
+                          appState.selectedCategory = null
                         }}>
-                      </Item>
-                    ))}
-                  </CategoryDetail>
+                        <CategoryForm newCategoryState={editCatState} cats={appState.allowedParentsForSelectedCategory} />
+                      </EditDeletePanel> : null
+                    }
+
+                  </Paper>
                 </Grid>
               }
             </Grid>
