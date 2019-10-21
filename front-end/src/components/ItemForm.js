8831eb7a17d169926184ddf313284091dfd11ffb
flexiblefactory <hal@hals-mbp.home>
2019-10-21 13:46:05 +0100

    Reformat

--- front-end/src/components/ItemForm.js
+++ front-end/src/components/ItemForm.js
@@ -7,62 +7,62 @@ import FormControl from '@material-ui/core/FormControl';
 import { observer } from "mobx-react"
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        padding: theme.spacing(3, 2),
-        margin: theme.spacing(1),
-    },
-    container: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    formControl: {
-        margin: theme.spacing(1),
-        minWidth: 120,
-        display: 'block'
-    },
-    textField: {
-        margin: theme.spacing(1),
-    },
-    dense: {
-        marginTop: theme.spacing(2),
-    },
-    menu: {
-        width: 200,
-    },
+  root: {
+    padding: theme.spacing(3, 2),
+    margin: theme.spacing(1),
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    display: 'block'
+  },
+  textField: {
+    margin: theme.spacing(1),
+  },
+  dense: {
+    marginTop: theme.spacing(2),
+  },
+  menu: {
+    width: 200,
+  },
 }));
 export default observer(({ cats, newItemState: [values, setValues] }) => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const handleChange = name => event => {
-        setValues({ ...values, [name]: event.target.value });
-    };
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value });
+  };
 
-    return (
-        <React.Fragment>
-            <FormControl className={classes.formControl}>
-                <TextField
-                    id="standard-name"
-                    label="Name"
-                    className={classes.textField}
-                    value={values.label}
-                    onChange={handleChange('label')}
-                    margin="normal"
-                />
-            </FormControl>
-            <FormControl className={classes.formControl}>
-                <InputLabel htmlFor="item-cat">
-                    Category
+  return (
+    <React.Fragment>
+      <FormControl className={classes.formControl}>
+        <TextField
+          id="standard-name"
+          label="Name"
+          className={classes.textField}
+          value={values.label}
+          onChange={handleChange('label')}
+          margin="normal"
+        />
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="item-cat">
+          Category
             </InputLabel>
-                <Select
-                    native
-                    value={values.category || ''}
-                    onChange={handleChange('category')}
-                    inputProps={{
-                        name: 'category',
-                        id: 'item-cat',
-                    }}>
-                    {cats.all.map(c => <option key={c.id} value={c.id}>{c.label}</option>)}
-                </Select>
-            </FormControl>
-        </React.Fragment>)
+        <Select
+          native
+          value={values.category || ''}
+          onChange={handleChange('category')}
+          inputProps={{
+            name: 'category',
+            id: 'item-cat',
+          }}>
+          {cats.all.map(c => <option key={c.id} value={c.id}>{c.label}</option>)}
+        </Select>
+      </FormControl>
+    </React.Fragment>)
 })
